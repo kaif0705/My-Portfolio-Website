6c0427bc66be6d30ae2ebbb1b3d29ef125d97f72
kaif0705 <mohammedkaifkilledar5@gmail.com>
2025-09-29 14:50:43 +0530

    Created custom animated cursor effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './app.scss'
+import { Cursor } from './Components/Cursor/Cursor';
 import { Hero } from './Components/Hero/Hero';
 import { Navbar } from './Components/Navbar/NAvbar';
+import { Parallax } from './Components/Parallax/Parallax';
 
 const App = () => {
   return(
     <>
       <div>
+        <Cursor />
         <section id='Homepage'>
           <Navbar />
           <Hero />
         </section>
-        <section id='Services'>Parallax</section>
+        <section id='Services'><Parallax type="services"/></section>
         <section>Services</section>
-        <section id='Portfolio'>Parallax</section>
+        <section id='Portfolio'><Parallax  type="portfolio"/></section>
         <section>Portfolio1</section>
         <section>Portfolio2</section>
         <section>Portfolio3</section>
